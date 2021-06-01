7235b801b0d4bdf696d36710a5bfa1bb7c1d885f
matijatomc <55134628+matijatomc@users.noreply.github.com>
2021-06-01 12:15:26 +0200

    Add files via upload

--- brod/brod.ino
+++ brod/brod.ino
@@ -5,9 +5,11 @@
 #include <Servo.h>
 
 // Oderđivanje na kojim se pinovima nalazi CE i CSN pinovi modula
-RF24 radio(7, 8); 
+RF24 radio1(7, 8); 
+RF24 radio2(7, 8); 
 // Određivanje kanala preko kojeg će moduli komunicirat
-const byte address[6] = {"00001"}; 
+const byte address1[6] = {"00001"}; 
+const byte address2[6] = {"00002"};
 Servo myServo; // Deklariranje serva 
 Servo ESC; // Deklariranje kontrolera za motor ESC
 int kutSkretanja = 0; // Deklariranje varijable za kut skretanja
@@ -19,28 +21,32 @@ void setup() {
   myServo.attach(5); // Deklaracija pina od serva
   // Deklaracija pina od ESC, minimalna širina impulsa, maksimalna širina impulsa
   ESC.attach(6,1000,2000); 
-  radio.begin(); // Započinjanje radio veze
+  radio1.begin(); // Započinjanje radio veze
+  radio2.begin();
   // Postavljanje adrese od kanala za prijemnik preko kojeg će komunicirat
-  radio.openReadingPipe(0, address); 
+  radio1.openReadingPipe(0, address1); 
+  radio2.openReadingPipe(1, address2);
   // Postavljanje maksimalne razine pojačanja snage 
-  radio.setPALevel(RF24_PA_MAX); 
+  radio1.setPALevel(RF24_PA_MAX); 
+  radio2.setPALevel(RF24_PA_MAX);
 }
 
 // Void koji će se vrtiti cijelo vrijeme dok Arduino radi
 void loop() {
-  radio.startListening(); // Postavljanje modula kao prijemink
-  if ( radio.available()) { // Provjera dali je radio veza uspostavljana
+  radio1.startListening(); // Postavljanje modula kao prijemink
+  radio2.startListening(); 
+  if ( radio1.available() || radio2.available()) { // Provjera dali je radio veza uspostavljana
     // Vrti sljedeču petlju sve dok je radio veze uspostavljana
-    while (radio.available()) {
+    while (radio1.available() || radio2.available()) {
 
       // Slušanje i spremanje vrijednost
-      radio.read(&kutSkretanja, sizeof(kutSkretanja)); 
+      radio1.read(&kutSkretanja, sizeof(kutSkretanja)); 
       // Pomicanje serva za primljenu vrijednost
       myServo.write(kutSkretanja);  
       Serial.print("Skretanje: "); Serial.println(kutSkretanja);
 
       // Slušanje i spremanje vrijednost 
-      radio.read(&vrijednostBrzine, sizeof(vrijednostBrzine)); 
+      radio2.read(&vrijednostBrzine, sizeof(vrijednostBrzine)); 
       // Postavljanje brzine motora za primljanu vrijednost
       ESC.write(vrijednostBrzine);
       Serial.print("Brzina: "); Serial.println(vrijednostBrzine);
